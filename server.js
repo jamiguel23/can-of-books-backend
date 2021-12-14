254bec6fa0ede7023d1b79dc61ad64ab53145fe1
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-13 16:12:56 -0800

    finished book model

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose')
 
 const app = express();
 app.use(cors());
@@ -14,5 +15,13 @@ app.get('/test', (request, response) => {
   response.send('test request received')
 
 })
+
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection;
+db.on('error' , console.error.bind(console, 'connection error'));
+db.once('open', function() {
+  console.log('Mongoose is connected playa')
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
